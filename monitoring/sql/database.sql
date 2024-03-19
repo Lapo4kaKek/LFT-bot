d6ad6ac324467ad248f7339e25ecef055b0c2e8e
Alexey Antoshkin <Antoshkin.Alexey.A@gmail.com>
2024-03-19 13:43:55 +0300

    add strategies table in db

--- monitoring/sql/database.sql
+++ monitoring/sql/database.sql
@@ -2,15 +2,29 @@ CREATE TABLE IF NOT EXISTS orders (
     orderId String,
     exchange String,
     symbol String,
-    price Float32,
-    qty Float32,
-    executedQty Float32,
-    totalCost Float32,
+    price Decimal(38, 20),
+    qty Decimal(38, 20),
+    executedQty Decimal(38, 20),
+    totalCost Decimal(38, 20),
     side String,
     orderType String,
     orderStatus String,
     createdTime DateTime,
     updatedTime DateTime,
-    commission Float32
+    commission Decimal(38, 20)
 ) ENGINE = MergeTree()
 ORDER BY (createdTime, orderId);
+
+
+CREATE TABLE IF NOT EXISTS strategies (
+    strategyId String,
+    name String,
+    exchange String,
+    symbol String,
+    balance Decimal(38, 20),
+    activeTokens Decimal(38, 20),
+    assetsNumber Decimal(38, 20),
+    status Boolean,
+    createdTime DateTime
+) ENGINE = MergeTree()
+ORDER BY (createdTime);
